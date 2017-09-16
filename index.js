7559490f2fb87ebfe94084560634d86d3cc9c202
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-16 21:59:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians, instruments) {
+  var arr = []
+  for(var i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + " plays " + instruments[i])
+  }
+  return arr
+}
+
+function johnLennonFacts(facts) {
+  var newFacts = []
+  var i = 0
+  while(facts.length > i) {
+    newFacts.push(facts[i] + '!!!')
+    --facts.length
+  }
+  return newFacts
+}
