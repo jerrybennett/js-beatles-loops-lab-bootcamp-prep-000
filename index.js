12627aebe7c2e7fa5bc22e7391e6d95e7ed7b486
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-16 22:11:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function iLoveTheBeatles(number) {
   var arr = []
   do {
     arr.push("I love the Beatles!")
-  } while (arr.length < 15 ? "I love the Beatles!");
+  } while (arr.length < 15);
   return arr
 }
